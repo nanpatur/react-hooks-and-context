1f6aa50b5a50b4a326ebcedec9ac703560730ae1
Nandi <nandirasp@sellfazz.com>
2019-11-05 20:22:10 +0700

    exercise part 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# react-hooks-and-context
-Exercise using React Hooks and Context
+Exercise React Hooks and Context
+
+## Tutorial Link
+- https://medium.com/octopus-labs-london/replacing-redux-with-react-hooks-and-context-part-1-11b72ffdb533
+
+
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Store } from './Store';
+
+export default function App() {
+  const { state, dispatch } = React.useContext(Store);
+  const fetchDataAction = async () => {
+    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes');
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    });
+  };
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+  return (
+    <React.Fragment>
+      {console.log(state)}
+      <div>
+        <h1>Rick and Morty</h1>
+        <p>Pick your favourite episodes</p>
+        <section>
+          {state.episodes.map(episode => {
+            return (
+              <section key={episode.id}>
+                <img
+                  src={episode.image ? episode.image.medium : ''}
+                  alt={`Rick and Morty ${episode.name}`}
+                />
+                <div>{episode.name}</div>
+                <section>
+                  <div>
+                    Season: {episode.season} Number: {episode.number}
+                  </div>
+                </section>
+              </section>
+            );
+          })}
+        </section>
+      </div>
+    </React.Fragment>
+  );
+}
