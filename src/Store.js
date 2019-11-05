bea9e1e4baa754ec1f387bf30dabf897a233bc36
Nandi <nandirasp@sellfazz.com>
2019-11-05 20:39:40 +0700

    styling and toggle function

--- src/Store.js
+++ src/Store.js
@@ -11,6 +11,16 @@ function reducer(state, action) {
   switch (action.type) {
     case 'FETCH_DATA':
       return { ...state, episodes: action.payload };
+    case 'ADD_FAV':
+      return {
+        ...state,
+        favourites: [...state.favourites, action.payload]
+      };
+    case 'REMOVE_FAV':
+      return {
+        ...state,
+        favourites: action.payload
+      };
     default:
       return state;
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { StoreProvider } from './Store';
-// import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
   <StoreProvider>
